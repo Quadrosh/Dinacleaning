6204194d47d3d588f7406e0ff41c5ac3f410c9d3
Quadrosh <quadrosh@gmail.com>
2017-05-28 01:49:47 +0300

    month fix

--- rest/controllers/CalendarController.php
+++ rest/controllers/CalendarController.php
@@ -43,13 +43,31 @@ class CalendarController extends ActiveController
     public function prepareDataProvider()
     {
         setlocale(LC_ALL, 'ru_RU.UTF-8');
-        $today = strftime('%A %e %B', time());
-        $date1 = strftime('%A %e %B', strtotime('+1 day'));
-        $date2 = strftime('%A %e %B', strtotime('+2 day'));
-        $date3 = strftime('%A %e %B', strtotime('+3 day'));
-        $date4 = strftime('%A %e %B', strtotime('+4 day'));
-        $date5 = strftime('%A %e %B', strtotime('+5 day'));
-        $date6 = strftime('%A %e %B', strtotime('+6 day'));
+
+        $months = array(
+            '01' => 'января', '02' => 'февраля', '03' => 'марта', '04' => 'апреля',
+            '05' => 'мая', '06' => 'июня', '07' => 'июля', '08' => 'августа',
+            '09' => 'сентября', '10' => 'октября', '11' => 'ноября', '12' => 'декабря'
+        );
+
+        //        $date1 = strftime('%A %e %B', strtotime('+1 day')) . $months[strftime('%m', strtotime('+1 day'))];
+
+
+        $today = strftime('%A %e ', time()) . $months[strftime('%m', time())];
+        $date1 = strftime('%A %e ', strtotime('+1 day')) . $months[strftime('%m', strtotime('+1 day'))];
+        $date2 = strftime('%A %e ', strtotime('+2 day')) . $months[strftime('%m', strtotime('+2 day'))];
+        $date3 = strftime('%A %e ', strtotime('+3 day')) . $months[strftime('%m', strtotime('+3 day'))];
+        $date4 = strftime('%A %e ', strtotime('+4 day')) . $months[strftime('%m', strtotime('+4 day'))];
+        $date5 = strftime('%A %e ', strtotime('+5 day')) . $months[strftime('%m', strtotime('+5 day'))];
+        $date6 = strftime('%A %e ', strtotime('+6 day')) . $months[strftime('%m', strtotime('+6 day'))];
+
+//        $today = strftime('%A %e %B', time());
+//        $date1 = strftime('%A %e %B', strtotime('+1 day'));
+//        $date2 = strftime('%A %e %B', strtotime('+2 day'));
+//        $date3 = strftime('%A %e %B', strtotime('+3 day'));
+//        $date4 = strftime('%A %e %B', strtotime('+4 day'));
+//        $date5 = strftime('%A %e %B', strtotime('+5 day'));
+//        $date6 = strftime('%A %e %B', strtotime('+6 day'));
 
 
         $dataProvider = [
