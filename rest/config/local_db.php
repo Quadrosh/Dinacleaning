bf98623a53e1665cce3cb5c8dc5c1caf31a19c6a
Quadrosh <quadrosh@gmail.com>
2017-11-18 12:03:37 +0300

    local params 0.6

--- rest/config/local_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=dinacleaning',
-    'username' => 'dinacleaningsql',
-    'password' => '7T4w4A1w',
-    'charset' => 'utf8',
-];
